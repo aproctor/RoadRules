15d9d242e05c2c4e2b0c88def68c4ebc10196590
Adam Proctor <adam@uken.com>
2018-05-04 15:18:19 -0400

    some assertions

--- Assets/Editor/Tests/CommandProcessorTests.cs
+++ Assets/Editor/Tests/CommandProcessorTests.cs
@@ -10,7 +10,23 @@ namespace RoadRules {
     [Test]
     public void TestCommandsList() {
       Assert.True(CommandProcessor.ValidateInstruction("forward"));
+      Assert.True(CommandProcessor.ValidateInstruction("forward 1"));
+      Assert.True(CommandProcessor.ValidateInstruction("forward -1"));
+      Assert.True(CommandProcessor.ValidateInstruction("forward 100"));
+      //Assert.False(CommandProcessor.ValidateInstruction("forward 100 40"));
+      Assert.False(CommandProcessor.ValidateInstruction("forward qq"));
+
       Assert.True(CommandProcessor.ValidateInstruction("wait"));
+      Assert.True(CommandProcessor.ValidateInstruction("wait 1"));
+      Assert.True(CommandProcessor.ValidateInstruction("wait -1"));
+      Assert.True(CommandProcessor.ValidateInstruction("wait 100"));
+      //Assert.False(CommandProcessor.ValidateInstruction("wait 100 40"));
+      Assert.False(CommandProcessor.ValidateInstruction("wait qq"));
+
+      Assert.True(CommandProcessor.ValidateInstruction("left"));
+      Assert.True(CommandProcessor.ValidateInstruction("right"));
+
+      Assert.False(CommandProcessor.ValidateInstruction("fubar"));
     }
   }
 }
--- Assets/_RoadRulesGame/Scripts/CommandProcessor.cs
+++ Assets/_RoadRulesGame/Scripts/CommandProcessor.cs
@@ -7,14 +7,18 @@ namespace RoadRules {
   public static class CommandProcessor {
 
     public static Regex[] COMMAND_LIST = {
-    new Regex("forward"),
-    new Regex("wait"),
-  };
+      new Regex(@"^forward(\d+)?$"),
+      new Regex(@"^wait( \d+)?$"),
+      new Regex("^right$"),
+      new Regex("^left$")
+    };
 
     public static bool ValidateInstruction(string instruction) {
+      Debug.LogFormat("Validating: {0}",instruction);
       foreach (Regex command in COMMAND_LIST) {
         Match match = command.Match(instruction);
         if (match != null) {
+          Debug.LogError("Match <"+match.ToString()+">");
           return true;
         }
       }
