3e1d29a42579547a22683899ab26d14de06ed831
Adam Proctor <adam@uken.com>
2018-05-04 16:19:01 -0400

    splitting

--- Assets/_RoadRulesGame/Scripts/CommandProcessor.cs
+++ Assets/_RoadRulesGame/Scripts/CommandProcessor.cs
@@ -6,6 +6,9 @@ using System.Text.RegularExpressions;
 namespace RoadRules {
   public static class CommandProcessor {
 
+    public static string HALT = "halt";
+    private static char[] SPACE_SPLIT = { ' ' };
+
     public static Regex[] COMMAND_LIST = {
       new Regex(@"^forward( -?\d+)?$"),
       new Regex(@"^wait( -?\d+)?$"),
@@ -13,15 +16,33 @@ namespace RoadRules {
       new Regex("^left$")
     };
 
+    public class Command {
+      public string instruction = null;
+      public int arg0 = 0;
+
+      public Command(string commandStr) {
+        string[] parts = commandStr.Split(SPACE_SPLIT);
+        instruction = parts[0];
+        if(parts.Length > 1) {
+          arg0 = System.Convert.ToInt32(parts[1]);
+        }
+      }
+    }
+
     public static bool ValidateInstruction(string instruction) {
       Debug.LogFormat("Validating: {0}",instruction);
+      return ParseCommand(instruction) != null;
+    }
+
+    public static Command ParseCommand(string instruction) {
       foreach (Regex command in COMMAND_LIST) {
         Match match = command.Match(instruction);
         if (match != null && match.Success) {
-          return true;
+          return new Command(instruction);
         }
       }
-      return false;
+
+      return null;
     }
 
 
--- Assets/_RoadRulesGame/Scripts/GameAgents/CommandableAgent.cs
+++ Assets/_RoadRulesGame/Scripts/GameAgents/CommandableAgent.cs
@@ -7,12 +7,20 @@ namespace RoadRules {
 
   public class CommandableAgent : MonoBehaviour {
 
+    private static char[] NEW_LINE_SPLIT = { '\n' };
+
+    public float moveSpeed = 1f;
+
+    [Header("Instructions")]
     [TextArea]
     public string instructionInput;
     private int instructionIndex = 0;
-    private ArrayList instructions;
+    private string lastInstruction = "";
+    private int repeatCount = 0; //How many times we'll repeat the last instruction
+    private List<string> instructions;
 
-    [Header("Triggers")]
+    [Header("Statefullness")]
+    private bool alive = true;
     public UnityEvent OnReset;
     public UnityEvent OnDie;
 
@@ -22,13 +30,15 @@ namespace RoadRules {
     private void Awake() {
       this.originalPosition = this.transform.position;
       this.originalRotation = this.transform.rotation;
+
+      //TODO call this from an external method
       Begin();
     }
 
     public void Begin() {
       //Parse instructions
-      string[] lines = instructionInput.Split(new char[] { '\n' });
-      instructions = new ArrayList();
+      string[] lines = instructionInput.Split(NEW_LINE_SPLIT);
+      instructions = new List<string>();
       foreach (string line in lines) {        
         string trimmed = line.Trim().ToLower();
         if(trimmed.Length == 0 || trimmed.StartsWith("#")) {
@@ -38,13 +48,32 @@ namespace RoadRules {
           instructions.Add(trimmed);
         } else {
           Debug.LogWarningFormat("Ignoring invalid instruction <{0}>",trimmed);
+          instructions.Add(CommandProcessor.HALT);
         }
       }
-      Debug.Log("Lines " + instructions.Count);
+    }
+
+    public void Tick() {
+      if (alive) {
+        if (repeatCount > 0) {
+          Run(lastInstruction);
+        } else if (instructionIndex < instructions.Count) {
+          //if(parts[0] == "forward") {
+            
+          //}
+        } else {
+          Debug.Log("Done instructions waiting", this);
+        }
+      }
+    }
+
+    private void Run(string instruction) {
+      
     }
 
 
     public void Die() {
+      alive = false;
       OnDie.Invoke();
     }
 
@@ -52,6 +81,8 @@ namespace RoadRules {
       this.transform.position = originalPosition;
       this.transform.rotation = originalRotation;
       instructionIndex = 0;
+      repeatCount = 0;
+      alive = true;
       OnReset.Invoke();
     }
 
