a5a5bc932910f8bbbdae575ede98e9003207a82a
Adam Proctor <adam@uken.com>
2018-05-06 13:44:35 -0400

    buttons

--- Assets/_RoadRulesGame/UI/HyperText.cs
+++ Assets/_RoadRulesGame/UI/HyperText.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class HyperText : MonoBehaviour {
 
   [Range(0.0f, 1f)]
-  public float tickRate = 0.1f;
+  public float tickRate = 0.33f;
   public Color[] pallete;
 
   private Text textField;
@@ -33,7 +33,18 @@ public class HyperText : MonoBehaviour {
     }
 	}
 
+  int lastIndex = -1;
+  Color NextColor() {
+    int index = Random.Range(0, pallete.Length);
+    if(index != lastIndex) {
+      lastIndex = index;
+      return pallete[index];  
+    }
+    //recursion for lazy rerolling.  Statistically this shouldn't be too many iterations before a valid new index is found
+    return NextColor();
+  }
+
   void Shift() {
-    textField.color = pallete[Random.Range(0,pallete.Length)];
+    textField.color = NextColor();
   }
 }
